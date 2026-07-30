728e2bd5fb38a4b4654f9ab3346253288402e86b
Dimitry Pianykh <111515533+DimitryP6@users.noreply.github.com>
2026-07-30 19:27:06 +0300

    Add: merge-strings-alternately - submission-6

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-6.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string result;
+        int stringIndex = 0;
+        for (stringIndex = 0; (stringIndex < word1.size()) && (stringIndex < word2.size()); stringIndex++) {
+            result += word1[stringIndex];
+            result += word2[stringIndex];
+        }
+        if (stringIndex < word1.size()) {
+            result.append(word1.substr(stringIndex));
+        } else {
+            result.append(word2.substr(stringIndex));
+        }
+        return result;
+    }
+};
