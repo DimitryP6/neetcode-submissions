d82133d497e0e621533117a3a01edd2f0661739f
Dimitry Pianykh <111515533+DimitryP6@users.noreply.github.com>
2026-08-01 16:43:16 +0300

    Add: three-integer-sum - submission-14

---
+++ Data Structures & Algorithms/three-integer-sum/submission-14.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> result;
+        for (int i = 0; i < nums.size() - 2; i++) {
+            if ((i > 0) && (nums[i] == nums[i - 1])) {
+                continue;
+            }
+            int remainder = 0 - nums[i];
+            int leftIndex = i + 1, rightIndex = nums.size() - 1;
+            while (leftIndex < rightIndex) {
+                if ((nums[leftIndex] + nums[rightIndex]) == remainder) {
+                    result.push_back({nums[i], nums[leftIndex], nums[rightIndex]});
+                    leftIndex++, rightIndex--;
+                    while ((leftIndex < rightIndex) && (nums[leftIndex] == nums[leftIndex - 1])) {
+                        leftIndex++;
+                    }
+                    while ((rightIndex > leftIndex) && (nums[rightIndex] == nums[rightIndex + 1])) {
+                        rightIndex--;
+                    }
+                } else if ((nums[leftIndex] + nums[rightIndex]) < remainder) {
+                    leftIndex++;
+                } else {
+                    rightIndex--;
+                }
+            }
+        }
+        return result;
+    }
+};
